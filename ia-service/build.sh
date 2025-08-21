1934de3a94ef8f9beda24e6b2ee64ed37a8672f9
MatheusNLima <muitomato@gmail.com>
2025-08-21 17:54:53 -0300

    refactor build script to create and activate virtual environment

--- ia-service/build.sh
+++ ia-service/build.sh
@@ -2,6 +2,10 @@
 # Exit on error
 set -o errexit
 
+# Create and activate virtual environment with Python 3.9
+python3.9 -m venv venv
+source venv/bin/activate
+
 # Install dependencies using pip
 pip install -r requirements.txt
 
